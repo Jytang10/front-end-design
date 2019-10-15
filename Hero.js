4001e85b323f48efe2805b6bf8aea67db0bf05ca
James Tang <jytang10@gmail.com>
2019-10-14 18:52:52 -0700

    Added LinearGradient

--- Hero.js
+++ Hero.js
@@ -1,14 +1,16 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, ImageBackground } from 'react-native';
-
+import {LinearGradient} from 'expo-linear-gradient';
 // create a component
 class Hero extends Component {
   render() {
     return (
       <View style={{marginTop:10}}>
         <ImageBackground imageStyle={{borderRadius:10}} style={{height:250}} source={{uri:"https://firebasestorage.googleapis.com/v0/b/mytry-bea8d.appspot.com/o/thumb-10.jpg?alt=media&token=9c9a3f0a-fb7c-4588-b5e3-412554f0a6b4"}}>
-          <Text style={{fontSize:22, fontWeight:'bold', color:'#fff'}}>Hero</Text>
+          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,1)']}>
+            <Text style={{fontSize:22, fontWeight:'bold', color:'#fff'}}>Hero</Text>
+          </LinearGradient>
         </ImageBackground>
       </View>
     );
