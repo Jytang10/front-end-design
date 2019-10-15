b7a7632b647b7ec18f30a4ccb6253fb49fde4d13
James Tang <jytang10@gmail.com>
2019-10-14 17:12:01 -0700

    Created Hero component

---
+++ Hero.js
@@ -0,0 +1,26 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+
+// create a component
+class Hero extends Component {
+  render() {
+    return (
+      <View style={{marginTop:10}}>
+        <ImageBackground imageStyle={{borderRadius:10}} style={{height:250}} source={{uri:"https://firebasestorage.googleapis.com/v0/b/mytry-bea8d.appspot.com/o/thumb-10.jpg?alt=media&token=9c9a3f0a-fb7c-4588-b5e3-412554f0a6b4"}}>
+          <Text style={{fontSize:22, fontWeight:'bold', color:'#fff'}}>Hero</Text>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
+//make this component available to the app
+export default Hero;
