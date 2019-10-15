2af7779645168133d6861a63a0a08b9dae0110e3
James Tang <jytang10@gmail.com>
2019-10-14 20:35:56 -0700

    FlatList styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, FlatList, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList, Image, Dimensions } from 'react-native';
 import Heading from './Heading';
 import Hero from './Hero';
 import LatestMembers from './LatestMembers';
@@ -36,13 +36,13 @@ class App extends Component {
             style={{width:'100%'}}
             data={this.state.data}
             keyExtractor={(item, index) => index.toString()}
-            renderItems={({item}) => (
+            renderItem={({item}) => (
               <View style={styles.rowStyle}>
                 <View style={styles.imageContainer}>
-                  <Image source={item.image_link}></Image>
+                  <Image style={styles.image} source={{uri:item.image_link}}></Image>
                 </View>
-                <View>
-                  
+                <View style={styles.rightSideContent}>
+                  <Text>{item.headline}</Text>
                 </View>
               </View>
             )}
@@ -60,10 +60,22 @@ const styles = StyleSheet.create({
   rowStyle: {
     flexDirection:'row',
     borderRadius:6,
-    backgroundColor:"#a4b0be"
+    backgroundColor:"#a4b0be",
+    padding:5,
+    margin:5
   },
   imageContainer: {
-
+    height:120,
+    width:(Dimensions.get('window').width / 3) - 4
+  },
+  image: {
+    flex:1,
+    width:null,
+    alignSelf:'stretch',
+    borderRadius:10
+  },
+  rightSideContent:{
+    width:(Dimensions.get('window').width / 1.5)
   }
 });
 
