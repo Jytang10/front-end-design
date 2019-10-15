aeff5e511b58d0c61b4fa2743e1148ebf58f7056
James Tang <jytang10@gmail.com>
2019-10-14 20:20:23 -0700

    Completed styling Mosaic

--- Mosaic.js
+++ Mosaic.js
@@ -15,8 +15,10 @@ class Mosaic extends Component {
     return this.state.data.map(singleData => {
       return (
         <View key={singleData.id} style={{height:250, margin:3, flexBasis:'48%'}}>
-          <ImageBackground style={{height:200}} source={{uri:singleData.image_link}}>
-            <Text style={{fontWeight:'bold', fontSize:15, paddingLeft:15, color:'#fff'}}>{singleData.headline}</Text>
+          <ImageBackground imageStyle={{borderRadius:20}} style={{height:200, position:'absolute', top:0, bottom:0, right:0, left:0}} source={{uri:singleData.image_link}}>
+            <LinearGradient style={{height:200, position:'absolute', top:0, bottom:0, right:0, left:0, flexDirection:'row', borderRadius:20}} start={{x: 0.1, y: 0.6}} end={{x: 0.1, y: 1}} colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,1)']}>
+              <Text style={{fontWeight:'bold', fontSize:15, paddingLeft:15, color:'#fff', alignSelf:'flex-end', paddingBottom:5}}>{singleData.headline}</Text>
+            </LinearGradient>
           </ImageBackground>
         </View>
       )
@@ -38,7 +40,8 @@ const styles = StyleSheet.create({
     height:800,
     overflow:'hidden',
     flexWrap:'wrap',
-    flexDirection:'row'
+    flexDirection:'row',
+    justifyContent:'space-between'
   },
 });
 
