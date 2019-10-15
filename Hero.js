286dd4768f12437df7a2936cb85370a9d78ad88c
James Tang <jytang10@gmail.com>
2019-10-14 18:59:12 -0700

    Completed styling for Hero

--- Hero.js
+++ Hero.js
@@ -8,8 +8,8 @@ class Hero extends Component {
     return (
       <View style={{marginTop:10}}>
         <ImageBackground imageStyle={{borderRadius:10}} style={{height:250}} source={{uri:"https://firebasestorage.googleapis.com/v0/b/mytry-bea8d.appspot.com/o/thumb-10.jpg?alt=media&token=9c9a3f0a-fb7c-4588-b5e3-412554f0a6b4"}}>
-          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,1)']}>
-            <Text style={{fontSize:22, fontWeight:'bold', color:'#fff'}}>Hero</Text>
+          <LinearGradient style={{flexDirection:'row', borderRadius:10, position:'absolute', left:0, right:0, bottom:0, top:0}} start={{x: 0.1, y: 0.6}} end={{x: 0.1, y: 1}} colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,1)']}>
+            <Text style={{fontSize:22, fontWeight:'bold', color:'#fff', alignSelf:'flex-end', paddingLeft:20, paddingBottom:20}}>Hot new Hero Image</Text>
           </LinearGradient>
         </ImageBackground>
       </View>
