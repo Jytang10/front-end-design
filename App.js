b5cf4b1dbb70521153f1a8420f4c28978afc30e5
James Tang <jytang10@gmail.com>
2019-10-14 20:27:29 -0700

    Added FlatList

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList, Image } from 'react-native';
 import Heading from './Heading';
 import Hero from './Hero';
 import LatestMembers from './LatestMembers';
 import Mosaic from './Mosaic';
+import data from './data';
 
 // create a component
 class App extends Component {
+
+  state = {
+    data:data
+  }
+
   render() {
     return (
       <ScrollView>
@@ -23,6 +29,26 @@ class App extends Component {
             </Text>
           </View>
           <Mosaic></Mosaic>
+          <Text style={{fontWeight:'bold', fontSize:24, marginTop:10, paddingLeft:6}}>
+            News
+          </Text>
+          <FlatList 
+            style={{width:'100%'}}
+            data={this.state.data}
+            keyExtractor={(item, index) => index.toString()}
+            renderItems={({item}) => (
+              <View style={styles.rowStyle}>
+                <View style={styles.imageContainer}>
+                  <Image source={item.image_link}></Image>
+                </View>
+                <View>
+                  
+                </View>
+              </View>
+            )}
+          >
+
+          </FlatList>
         </View>
       </ScrollView>
     );
@@ -31,7 +57,14 @@ class App extends Component {
 
 // define your styles
 const styles = StyleSheet.create({
+  rowStyle: {
+    flexDirection:'row',
+    borderRadius:6,
+    backgroundColor:"#a4b0be"
+  },
+  imageContainer: {
 
+  }
 });
 
 //make this component available to the app
