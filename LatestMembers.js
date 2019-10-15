a541e186690f5a0224842650ee608a24ea188f4c
James Tang <jytang10@gmail.com>
2019-10-14 19:08:30 -0700

    Mapped through data

--- LatestMembers.js
+++ LatestMembers.js
@@ -1,13 +1,32 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import data from './data';
 
 // create a component
 class LatestMembers extends Component {
+
+  state = {
+    data:data
+  }
+
+  getLatestMembers = () => {
+    return this.state.data.map(singleData => {
+      return (
+        <View style={{}} key={singleData.id}>
+          <Text>{singleData.username}</Text>
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={{fontSize:22, paddingTop:15}}>Latest Members</Text>
+        <View>
+          {this.getLatestMembers()}
+        </View>
       </View>
     );
   }
